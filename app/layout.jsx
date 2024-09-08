5ecc54df0e71d542fb573e91db120fc44f19263a
Yashvanthk05 <yashvanthk05@gmail.com>
2024-09-08 23:57:43 +0530

    title

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,20 @@
+"use client"
 import React from 'react';
 import './globals.css';
+import {Helmet} from 'react-helmet';
 
 export default function RootLayout({ children }) {
   return (
+    <>
+    <Helmet>
+        <title>WePlant</title>
+        <meta name="description" content="AI Driven Crop Disease Prediction" />
+    </Helmet>
     <html lang="en">
       <body>
         {children}
       </body>
     </html>
+    </>
   );
 }
